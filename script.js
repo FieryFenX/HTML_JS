91ff7ccd09cee4c2942fd551debaaf42a0f6c33e
Selkov Dmitry <rizhiy1998@mail.ru>
2019-04-30 23:15:28 +0300

    Improvements and fixes

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-var p = document.getElementsByTagName('p')[0];
+var p = document.querySelector('p');
 var text = p.textContent;
 console.log(text);
 var obj = {
   element: text.match(/element:(\w+);/)[1].toLowerCase(),
   text: text.match(/text:(.+?);/)[1],
-  count: parseInt(text.match(/count:(\d+);/)[1])
+  count: parseInt(text.match(/count:(\d+|-\d+);/)[1])
 };
 console.log(obj);
 console.log("Tag: "+obj.element+"\nText: "+obj.text+"\nCount: "+obj.count);
 
-var body = document.getElementsByTagName('body')[0];
-var newP = document.createElement(obj.element);
-body.replaceChild(newP, p);
-newP.textContent = obj.text;
-for (var i = 1; i < parseInt(obj.count); i++)
-  body.insertBefore(body.children[0].cloneNode(true), body.children[0]);
+var body = document.body;
+var newTag = document.createElement(obj.element);
+newTag.textContent = obj.text;
+body.removeChild(p);
+for (var i = 0; i < obj.count; i++)
+  body.appendChild(newTag.cloneNode(true));
 console.log(body.innerHTML);
